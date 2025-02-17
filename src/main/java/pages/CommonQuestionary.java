c261c9f4f6aeb9c1d0ef23ce78968146f09e84e5
Akshay Shinde <akshay.shinde9522@gmail.com>
2025-02-17 10:02:20 +0530

    file changed

---
+++ src/main/java/pages/CommonQuestionary.java
@@ -0,0 +1,12 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+public interface CommonQuestionary {
+	
+void hasMessage();
+void selectCountryFromList();
+void enterYourAddress();
+void enterYourTaxDetails();
+}
---
+++ src/main/java/pages/DemoQuestionary.java
@@ -0,0 +1,29 @@
+package pages;
+
+public class DemoQuestionary implements CommonQuestionary{
+
+	@Override
+	public void hasMessage() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void selectCountryFromList() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void enterYourAddress() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void enterYourTaxDetails() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
